e8db632462561980fdc7f39baa1e8f77bffa76bf
Amit Upadhyay <code@amitu.com>
2015-11-18 13:27:29 +0530

    emacs: use-package and dependencies

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,17 @@
+(require 'package)
+(add-to-list 'package-archives
+    '("melpa-stable" . "https://stable.melpa.org/packages/") t
+)
+(package-initialize)
+
+(require 'use-package)
+
+(use-package projectile
+    :ensure t
+)
+
+(set 'use-package-verbose t)
+
 ; show how long it took to load emacs
 ; this must be the last few lines of this file
 ; if this goes out of hand http://www.emacswiki.org/emacs/ProfileDotEmacs
