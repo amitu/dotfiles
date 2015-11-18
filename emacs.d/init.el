6443513b05e2f06a79f5cf91c8f6715c8c4066a7
Amit Upadhyay <code@amitu.com>
2015-11-18 12:03:24 +0530

    minor

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,8 @@
+; show how long it took to load emacs
+; this must be the last few lines of this file
+; if this goes out of hand http://www.emacswiki.org/emacs/ProfileDotEmacs
 (fset 'startup-echo-area-message
-        #'(lambda ()
-                  (message "emacs loaded in %s" (emacs-init-time))))
+    #'(lambda ()
+          (message "emacs loaded in %s" (emacs-init-time))
+    )
+)
